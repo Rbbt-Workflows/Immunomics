b75277ebf31010b2fa2daf220dcafd2e2ae87e22
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-07-15 14:52:40 +0200

    nil

--- lib/Immunomics/tasks/vcf.rb
+++ lib/Immunomics/tasks/vcf.rb
@@ -1,5 +1,6 @@
 module Immunomics
 
+  input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"hg19" => "Hsa/feb2014", "hg38" => "Hsa/may2017"}
   dep Sample, :mi, :vcf => true
   dep_task :vcf_epitopes, Immunomics, :epitopes, :mutated_isoforms => :mi
 
