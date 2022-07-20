7639f8f2ff9e12c079e8065d3ba9a5b0a0fe3d36
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-07-20 15:43:03 +0200

    nil

--- lib/Immunomics/tasks/vcf.rb
+++ lib/Immunomics/tasks/vcf.rb
@@ -10,9 +10,11 @@ module Immunomics
   dep :vcf_mis
   dep_task :vcf_epitopes, Immunomics, :epitopes, :mutated_isoforms => :vcf_mis, :flank_size => 20
 
+  input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"Hsa/feb2014" => "hg19 (Hsa/feb2014)", "Hsa/may2017" => "hg38 (Hsa/may2017) "}
   dep Sequence, :genomic_mutations, :quality => nil, :filters => []
   dep_task :vcf_open_epitopes, Immunomics, :open_epitopes, :mutations => :genomic_mutations
 
+  input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"Hsa/feb2014" => "hg19 (Hsa/feb2014)", "Hsa/may2017" => "hg38 (Hsa/may2017) "}
   dep Sequence, :genomic_mutations, :quality => nil, :filters => []
   dep_task :vcf_open_intron_epitopes, Immunomics, :open_intron_epitopes, :mutations => :genomic_mutations
 end
