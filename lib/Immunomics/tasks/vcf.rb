53f4461463a912fbb3cfac92d70d46074f4bc43e
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-07-21 13:23:53 +0200

    nil

--- lib/Immunomics/tasks/vcf.rb
+++ lib/Immunomics/tasks/vcf.rb
@@ -7,14 +7,15 @@ module Immunomics
     step(:mutated_isoforms_fast).load.values.flatten.uniq
   end
 
-  dep :vcf_mis
-  dep_task :vcf_epitopes, Immunomics, :epitopes, :mutated_isoforms => :vcf_mis, :flank_size => 20
+  input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"Hsa/feb2014" => "hg19 (Hsa/feb2014)", "Hsa/may2017" => "hg38 (Hsa/may2017) "}
+  dep :vcf_mis, :organism => :organism
+  dep_task :vcf_epitopes, Immunomics, :epitopes, :mutated_isoforms => :vcf_mis, :flank_size => 20, :organism => :organism
 
   input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"Hsa/feb2014" => "hg19 (Hsa/feb2014)", "Hsa/may2017" => "hg38 (Hsa/may2017) "}
   dep Sequence, :genomic_mutations, :quality => nil, :filters => []
-  dep_task :vcf_open_epitopes, Immunomics, :open_epitopes, :mutations => :genomic_mutations
+  dep_task :vcf_open_epitopes, Immunomics, :open_epitopes, :mutations => :genomic_mutations, :organism => :organism
 
   input :organism, :select, "Organism code", "Hsa/may2017", :select_options => {"Hsa/feb2014" => "hg19 (Hsa/feb2014)", "Hsa/may2017" => "hg38 (Hsa/may2017) "}
   dep Sequence, :genomic_mutations, :quality => nil, :filters => []
-  dep_task :vcf_open_intron_epitopes, Immunomics, :open_intron_epitopes, :mutations => :genomic_mutations
+  dep_task :vcf_open_intron_epitopes, Immunomics, :open_intron_epitopes, :mutations => :genomic_mutations, :organism => :organism
 end
